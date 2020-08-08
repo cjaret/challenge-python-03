3c127062214c786c6440826b5ce821324a489a78
“Daniel <cjaimesretiguin@gmail.com>
2020-08-08 02:15:32 -0500

    challenge completed

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 # Resolve the problem!!
-
+import re
+import os
+import sys
 
 def run():
-    # Start coding here
+
+    minusculas = []
+    
+    with open(os.path.join(sys.path[0], "encoded.txt"), 'r', encoding="utf-8") as f:
+        
+        for line in f.readlines():
+            minusculas.append(re.findall("[a-z]", line))
+        
+        clave = ''.join(map(str, minusculas))
+        clave = re.findall("[a-z]", line)
+        clave = ''.join(clave)
+
+        print(clave)
 
 
 if __name__ == '__main__':
